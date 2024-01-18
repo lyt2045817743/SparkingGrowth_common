686eca5b75882ce522362b61994dc9013fe6e451
刘玉婷 <liuyuting7@xiaomi.com>
2024-01-18 14:32:22 +0800

    feat: 常量迁移

---
+++ constants/global.js
@@ -0,0 +1,11 @@
+export const PageTypeMap = {
+  Add: 1,
+  Edit: 2,
+  View: 3
+}
+
+export const PageTypeLabel = {
+  [PageTypeMap.Add]: '新增',
+  [PageTypeMap.Edit]: '编辑',
+  [PageTypeMap.View]: '详情',
+}
---
+++ constants/index.js
@@ -0,0 +1,4 @@
+export * from './global';
+export * from './point';
+export * from './course';
+export * from './todo';
