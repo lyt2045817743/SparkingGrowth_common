60038bec0e6b2420c28a4b28d8c72e1b2fd84315
刘玉婷 <liuyuting7@xiaomi.com>
2024-01-18 19:57:15 +0800

    feat: 完成待办公共逻辑

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as indexDBApi from './api';
 
 export * from './constants';
 export * from './utils';
+export * from './logic';
 
 export {
   indexDBApi
---
+++ logic/todo.js
@@ -0,0 +1,26 @@
+import { TodoStatusMap, TodoTypeScore, PointEventTypeMap, CycleMap } from '../constants'
+
+export function formatCompletedTodo(item) {
+  const { key: id, deadline, type, score, cycleType } = item;
+  const now = Date.now();
+  const isOverdue = now > new Date(deadline).getTime();
+  const status = isOverdue ? TodoStatusMap.DoneButOverdue : TodoStatusMap.Done;
+  const pointInfo = {
+    eventId: id,
+    eventType: PointEventTypeMap.Todo,
+    score: score !== undefined ? score : type.reduce((a, b) => a + TodoTypeScore[b], 0)
+  }
+
+  if (isOverdue) {
+    pointInfo.score = pointInfo.score / 2;
+  }
+
+  if (cycleType !== CycleMap.UnCycle) {
+    pointInfo.createTime = now;
+  }
+
+  return {
+    todoInfo: { status, finishTime: now },
+    pointInfo
+  }
+}
