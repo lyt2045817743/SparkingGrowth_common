94bc073a6fbe073209cf3ae0f30ea9776768b4ca
刘玉婷 <liuyuting7@xiaomi.com>
2024-01-30 16:27:19 +0800

    feat: 去掉积分逻辑

--- src/logic/todo.js
+++ src/logic/todo.js
@@ -1,4 +1,4 @@
-import { TodoStatusMap, TodoTypeScore, PointEventTypeMap, CycleMap } from '../constants'
+import { TodoStatusMap } from '../constants'
 
 
 /**
@@ -9,26 +9,12 @@ import { TodoStatusMap, TodoTypeScore, PointEventTypeMap, CycleMap } from '../co
  * @returns {object} { todoInfo, pointInfo }
 */
 export function formatCompletedTodo(item) {
-  const { key: id, deadline, type, score, cycleType } = item;
+  const { deadline } = item;
   const now = Date.now();
   const isOverdue = now > new Date(deadline).getTime();
   const status = isOverdue ? TodoStatusMap.DoneButOverdue : TodoStatusMap.Done;
-  const pointInfo = {
-    eventId: id,
-    eventType: PointEventTypeMap.Todo,
-    score: score !== undefined ? score : type.reduce((a, b) => a + TodoTypeScore[b], 0)
-  }
-
-  if (isOverdue) {
-    pointInfo.score = pointInfo.score / 2;
-  }
-
-  if (cycleType !== CycleMap.UnCycle) {
-    pointInfo.createTime = now;
-  }
 
   return {
     todoInfo: { status, finishTime: now },
-    pointInfo
   }
 }
