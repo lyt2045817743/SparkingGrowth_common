757795bb790f0af539318f8e052689cc1a3b17c5
刘玉婷 <liuyuting7@xiaomi.com>
2024-02-01 16:49:04 +0800

    feat: 书籍&课程获取数据api添加筛选条件

--- src/api/book.js
+++ src/api/book.js
@@ -5,10 +5,15 @@ export const BookStoreName = 'book';
 /**
  * @memberof api
  * @function getBookList
+ * @property {object} filters 过滤条件
+ * @property {number} filters.type 书籍类型
  * @returns {Array} 书籍列表数据
  */
-async function getBookList() {
-  const list = await db.getAll(BookStoreName);
+async function getBookList(filters) {
+  let list = await db.getAll(BookStoreName);
+  if (filters.type) {
+    list = list.filter((item) => item.type === filters.type);
+  }
   return Promise.resolve(list);
 }
 
--- src/api/course.js
+++ src/api/course.js
@@ -5,10 +5,15 @@ export const CourseStoreName = 'course';
 /**
  * @memberof api
  * @function getCourseList
+ * @property {object} filters 过滤条件
+ * @property {number} filters.type 课程类型
  * @returns {Array} 课程列表数据
  */
-async function getCourseList() {
-  const list = await db.getAll(CourseStoreName);
+async function getCourseList(filters) {
+  let list = await db.getAll(CourseStoreName);
+  if (filters.type) {
+    list = list.filter((item) => item.type === filters.type);
+  }
   return Promise.resolve(list);
 }
 
